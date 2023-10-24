42303a75572901f25c3aceb9bda3400725a292f5
Ismail Dev <ismailbak7@gmail.com>
2023-10-24 18:33:01 +0100

    Complete The Array Stack

--- src/main/java/com/ismail/coder/datastructures/ArrayStack.java
+++ src/main/java/com/ismail/coder/datastructures/ArrayStack.java
@@ -1,20 +1,57 @@
 package com.ismail.coder.datastructures;
 
-public class ArrayStack {
+import java.util.EmptyStackException;
+
+public class ArrayStack<T> {
     private int capacity;
     private int size = 0;
-    private int[] data;
+    private T[] data;
 
     public ArrayStack() {
         this(16);
     }
 
+    @SuppressWarnings("unchecked")
     public ArrayStack(int capacity) {
+        if (capacity <= 0)
+            throw new IllegalArgumentException();
         this.capacity = capacity;
-        this.data = new int[capacity];
+        this.data = (T[]) new Object[capacity];
+    }
+
+    public void push(T value) {
+        if (size >= capacity)
+            throw new IndexOutOfBoundsException(capacity);
+        data[size++] = value;
+    }
+
+    public T stackTop() {
+        if (size == 0)
+            return null;
+        return data[size - 1];
+
+    }
+
+    public T pop() {
+        if (size == 0)
+            throw new EmptyStackException();
+        var value = data[size - 1];
+        data[size - 1] = null;
+        size--;
+        return value;
     }
 
     public boolean isFull() {
         return size == capacity;
+    }
+
+    public T peek(int index) {
+        if (index < 0 || index >= size)
+            throw new IndexOutOfBoundsException(index);
+        return data[index];
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
     }
 }
