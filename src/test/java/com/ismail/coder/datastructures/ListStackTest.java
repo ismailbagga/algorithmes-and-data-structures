3adca895930fb3b853efdaa69d57fb306ff9a3a8
Ismail Dev <ismailbak7@gmail.com>
2023-10-26 19:26:46 +0100

    Test ListStack.java

---
+++ src/test/java/com/ismail/coder/datastructures/ListStackTest.java
@@ -0,0 +1,45 @@
+package com.ismail.coder.datastructures;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.EmptyStackException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ListStackTest {
+
+    private ListStack<Integer> emptyStack, fullStack;
+
+    @BeforeEach
+    public void setUp() {
+        emptyStack = new ListStack<>(1);
+        fullStack = new ListStack<>(3);
+        fullStack.push(1);
+        fullStack.push(2);
+        fullStack.push(3);
+    }
+
+    @Test
+    @DisplayName("Pushing Values To The Stack")
+    public void pushValue() {
+        assertTrue(emptyStack.isEmpty());
+        emptyStack.push(10);
+        assertThrows(StackOverflowError.class, () -> fullStack.push(100));
+        assertTrue(emptyStack.isFull());
+    }
+
+    @Test
+    @DisplayName("Pulling Values From The Stack")
+    public void pullValue() {
+        assertEquals(3, fullStack.pop());
+        assertEquals(2, fullStack.pop());
+        assertEquals(1, fullStack.pop());
+        assertTrue(fullStack.isEmpty());
+        assertThrows(EmptyStackException.class, () -> fullStack.pop());
+        assertThrows(EmptyStackException.class, () -> fullStack.pop());
+    }
+}
